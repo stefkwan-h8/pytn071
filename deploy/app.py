fe8d2b80a7d05b208a21a7f467d3efac100dfb58
stefkwan-h8 <schristine@hacktiv8.com>
2022-04-14 21:08:11 +0700

    sesi 15, flask

--- deploy/app.py
+++ deploy/app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+import pickle
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/campus-placement')
 def campus():
     return render_template("main.html")
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    fitur = []
+    for val in request.form.values():
+        fitur.append(int(val)) #[100, 0]
+
+    nilai_sma = "Nilai SMA: " + str(fitur[0])
+    jurusan = "Jurusan: " + ["Arts", "Commerce", "Science"][fitur[1]]
+
+    # reshape fitur jadi bentuk 2D, 1 kolom
+    fitur = [fitur] #[[100, 0]]
+
+    # bikin prediksi, kita load model kita
+    model = pickle.load(open("model/campus_tree.pkl", 'rb'))
+    hasil = model.predict(fitur)
+
+    index = hasil[0]
+    prediksi = "hasil prediksi: " + ["Tidak Diterima", "Diterima Kerja"][index]
+
+    # return nilai_sma + " " + jurusan + " " + prediksi
+    return render_template("main.html", nilai_sma=nilai_sma, jurusan=jurusan, prediksi=prediksi)
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello, World!'
